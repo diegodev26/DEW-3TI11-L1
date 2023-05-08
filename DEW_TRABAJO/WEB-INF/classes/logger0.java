7f83b44c071524237cc7b175780d2d5165255cf6
Diego Córdoba Serra <102562030+diegodev26@users.noreply.github.com>
2023-05-08 18:48:49 +0200

    Add files via upload

---
+++ DEW_TRABAJO/WEB-INF/classes/logger0.java
@@ -0,0 +1,52 @@
+
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class logger0
+ */
+public class logger0 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public logger0() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter pw = response.getWriter();
+		String usuario = request.getParameter("user");
+
+		String preTituloHTML5 = "<!DOCTYPE html>\n<html>\n<head>\n"
+		 + "<meta http-equiv=\"Content-type\" content=\"text/html; charset=utf-8\" />";
+		 response.setContentType("text/html");
+		pw.println(preTituloHTML5);
+				pw.println(LocalDateTime.now().toString() + " " + request.getQueryString() + " " + usuario + " " + request.getRemoteAddr() + " " + getServletName() + " " + request.getRequestURI() + " " + request.getMethod() +" \n");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
