ee4edc5e0d989fdab497d29709d0ee7b16a9b0ec
Diego Córdoba Serra <102562030+diegodev26@users.noreply.github.com>
2023-05-30 12:45:22 +0200

    Update Logger2.java

--- TRABAJO_DEW/src/java/Logger2.java
+++ TRABAJO_DEW/src/java/Logger2.java
@@ -15,8 +15,26 @@ public class Logger2 extends HttpServlet {
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		// CREACION DEL ARCHIVO DE TEXTO USANDO EL PARAMETRO DEL SERVLET LOGPATH Y CONTROL DE FALLO DE CREACION(ESTANDAR)
+		File f = new File(getServletContext().getInitParameter("logPath"));
+		try {
+			f.createNewFile();
+		}catch(Exception e) {
+			System.out.println("Error de creacion de archivo");
+		}
+		
+		// OBTENEMOS Y GUARDAMOS LAS VARIABLES ESCRITAS Y ENVIADAS DESDE EL FORMULARIO
+		String dni = request.getParameter("dni");
+		String nombre = request.getParameter("nombre");
+		String apellidos = request.getParameter("apellidos");
+		
+		// INICIALIZAMOS EL PRINT EN PANTALLA CON UBICACIÓN DE ESCRITURA EN EL PARAMETRO LOGPATH
+		PrintWriter pw = new PrintWriter(new FileOutputStream(new File(getServletContext().getInitParameter("logPath")),true));
+		
+		// ESCRIBIMOS LA INFORMACION EN EL ARCHIVO DE TEXTO
+		pw.println(LocalDateTime.now().toString() + " " + request.getQueryString() + " " + dni + ", " + nombre + ", " + apellidos + " " + request.getRemoteAddr() + " " + getServletName() + " " + request.getRequestURI() + " " + request.getMethod() +" \n");
+		pw.close();
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
