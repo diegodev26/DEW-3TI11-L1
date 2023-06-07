eef1b7e8f11647785365a1dbf12006b9ff17cf34
diegodev26 <diecorserra@gmail.com>
2023-06-07 21:17:34 +0200

    Fix Metodo para Solicitudes

--- Notas_Online/WEB-INF/classes/Authorization.java
+++ Notas_Online/WEB-INF/classes/Authorization.java
@@ -12,6 +12,8 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.apache.hc.client5.http.ClientProtocolException;
+import org.apache.hc.client5.http.classic.HttpClient;
+import org.apache.hc.client5.http.classic.methods.HttpGet;
 import org.apache.hc.client5.http.classic.methods.HttpPost;
 import org.apache.hc.client5.http.cookie.BasicCookieStore;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
@@ -42,11 +44,11 @@ public class Authorization implements Filter {
         // DEFINIMOS LOS SERVLETS GENERICOS CON EL PROTOCOLO HTTP
         HttpServletRequest http_req = (HttpServletRequest) request;
         HttpServletResponse http_resp = (HttpServletResponse) response;
-
+        
         // COMPROBACION DE QUE FUNCIONA EL FORMULARIO
         String user = http_req.getParameter("user");
         String password = http_req.getParameter("password");
-
+        
         // PrintWriter out = http_resp.getWriter();
         // out.println("usuario: " + user + " contraseña: " + password);
 
@@ -59,63 +61,60 @@ public class Authorization implements Filter {
         // AÑADO LOS PARAMETROS A LA SESION
         session.setAttribute("usr", user);
         session.setAttribute("pass", password);
-
+        
         // CREO EL JSON PARA HACER LA SOLICITUD Y AÑADO LA INFORMACION
         JSONObject json = new JSONObject();
         json.put("dni", user);
         json.put("password", password);
 
-        // DEFINO LA URL DE LA SOLICITUD POST ->
-        // http://dew-milogin-2223.dsicv.upv.es:9090/CentroEducativo/login
+        // DEFINO LA URL DE LA SOLICITUD POST -> http://dew-milogin-2223.dsicv.upv.es:9090/CentroEducativo/login 
         String milogin = http_req.getLocalName();
         String dir = "http://" + milogin + ":9090/CentroEducativo/login";
 
         // CREACION DEL CLIENTE Y POST DESTINO HTTP APACHE
-        try (final CloseableHttpClient cliente = HttpClients.custom()
-                .setDefaultCookieStore(cookies)
-                .build()) {
-            // CONSULTA POST
-            HttpPost post = new HttpPost(dir);
+        final CloseableHttpClient cliente = HttpClients.custom().setDefaultCookieStore(cookies).build();
+        
+        //------------------------------------------------------
+        // METODO DE CLIENTE GUARDADO EN SESION
+        // HttpClient cliente = HttpClients.custom().setDefaultCookieStore(cookies).build();
+        // session.setAttribute("cliente", cliente);
+    	//------------------------------------------------------
+        
+        // CONSULTA POST
+    	HttpPost post = new HttpPost(dir);
+    	
+    	// ENCAPSULO EL MENSAJE DE FORMATIO JSON, AÑADO ENTITY A EL PAQUETE DE ENVIO
+        String jsonString = json.toString();
+        // JSON COMPROBACION
+        //out.println(jsonString);
+        StringEntity entity = new StringEntity(jsonString, ContentType.APPLICATION_JSON);
+        post.setEntity(entity);
+        // EJECUTO LA SOLICITUD
+        CloseableHttpResponse execute = cliente.execute(post);
+        // COMPROBACION CONEXION
+    	//out.println(execute.getCode());
+        if(execute.getCode() != 200) {
+        	System.out.println("Parece que no ha sido posible establecer la conexion con el servidor");
+        	http_resp.sendRedirect("/Notas_Online/");
+        }else {System.out.println("Conexion realizada con exito");}
+        // CREAMOS LA KEY PARA LAS CONSULTAS
+        String key;
+		try {
+			key = EntityUtils.toString(execute.getEntity());
+	        session.setAttribute("key", key);
+		} catch (ParseException | IOException e) {
+			e.printStackTrace();
+		}
+		
+		session.setAttribute("cookies", cookies);
+    	// CERRAMOS RESPUESTA Y CLIENTE
+        execute.close();
+        // cliente.close();
 
-            // ENCAPSULO EL MENSAJE DE FORMATIO JSON, AÑADO ENTITY A EL PAQUETE DE ENVIO
-            String jsonString = json.toString();
-            // JSON COMPROBACION
-            // out.println(jsonString);
-            StringEntity entity = new StringEntity(jsonString, ContentType.APPLICATION_JSON);
-            post.setEntity(entity);
-            // EJECUTO LA SOLICITUD
-            try (final CloseableHttpResponse execute = cliente.execute(post)) {
-                // COMPROBACION CONEXION
-                // out.println(execute.getCode());
-                if (execute.getCode() != 200) {
-                    System.out.println("Parece que no ha sido posible establecer la conexion con el servidor");
-                    http_resp.sendRedirect("/Notas_Online/");
-                } else {
-                    System.out.println("Conexion realizada con exito");
-                }
-                // CREAMOS LA KEY PARA LAS CONSULTAS Y GUARDAMOS COOKIES
-                String key = EntityUtils.toString(execute.getEntity());
-                List<Cookie> cookielist = cookies.getCookies();
-                session.setAttribute("key", key);
-                session.setAttribute("cookielist", cookielist);
-                // CERRAMOS RESPUESTA Y CLIENTE
-                execute.close();
-                cliente.close();
-            } catch (ClientProtocolException e) {
-                e.printStackTrace();
-            } catch (ParseException e) {
-                e.printStackTrace();
-            }
-        } catch (ClientProtocolException e) {
-            e.printStackTrace();
-        }
         // ON ESTE METODO DE LOGIN POR FORMULARIO HE VISTO QUE CUANDO UN USUARIO NO ESTA
-        // AUTORIZADO, SE DEVULVE UN ERROR 406. AHORA SOLO TENGO QUE DEFINIR EN EL
-        // WEB.XML
-        // QUE CUANDO RECIVA UN ERROR 406, SE REDIRIJA AL CLIENTE A UNA PAGINA
-        // ESPECIFICA
-        // UNA VEZ COMPROBADO EL CODIGO, LA EJECUCION DE CHAIN PERMITE CONTINUAR A LA
-        // SOLICITUD
+        // AUTORIZADO, SE DEVULVE UN ERROR 406. AHORA SOLO TENGO QUE DEFINIR EN EL WEB.XML
+        // QUE CUANDO RECIVA UN ERROR 406, SE REDIRIJA AL CLIENTE A UNA PAGINA ESPECIFICA
+        // UNA VEZ COMPROBADO EL CODIGO, LA EJECUCION DE CHAIN PERMITE CONTINUAR A LA SOLICITUD
         chain.doFilter(request, response);
     }
 }
--- Notas_Online/WEB-INF/classes/MenuAlumno.java
+++ Notas_Online/WEB-INF/classes/MenuAlumno.java
@@ -1,6 +1,8 @@
 
+
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -8,31 +10,49 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.apache.hc.client5.http.classic.HttpClient;
+import org.apache.hc.client5.http.classic.methods.HttpGet;
 import org.apache.hc.client5.http.cookie.BasicCookieStore;
+import org.apache.hc.client5.http.cookie.Cookie;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.http.ParseException;
+import org.apache.hc.core5.http.io.entity.EntityUtils;
 
 public class MenuAlumno extends HttpServlet {
-    private static final long serialVersionUID = 1L;
-
+	private static final long serialVersionUID = 1L;
+       
     public MenuAlumno() {
         super();
     }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-
-        // CAPTURAMOS LA SESION ACTUAL DEL CLIENTE
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		// CAPTURAMOS LA SESION ACTUAL DEL CLIENTE
         HttpSession session = request.getSession();
 
-        // USAMOS BASICCOOKIE PARA CREAR COOKIES
+        // CREAMOS NUEVO ALMACEN DE COOKIES
         BasicCookieStore cookies = new BasicCookieStore();
+        // OBTENEMOS LISTA DE COOKIES ANTERIORES
+        BasicCookieStore cookies_lts = (BasicCookieStore) session.getAttribute("cookies");
+        List<Cookie> cookielist = cookies_lts.getCookies();
 
-        // CARGAMOS VARIABLES DE SESION
+        // CARGAMOS VARIABLES DE SESION, TODAS LAS COOKIES ANTERIORES EN LA NUEVA VARIABLE
+        // REVISAR METODO COOKIES, PUEDE NO TENER SETNIDO ESTE PROCESO
         String key = (String) session.getAttribute("key");
-        List<Cookie> cookielist = cookies.getCookies();
-        cookies.addCookie(cookielist);
+        String user = (String) session.getAttribute("usr");
+        for (Cookie cookie : cookielist) {
+        	cookies.addCookie(cookie);
+        }
 
         // OBJETOS
         final CloseableHttpClient cliente = HttpClients.custom().setDefaultCookieStore(cookies).build();
+        
+        //------------------------------------------------------
+        // METODO DE CLIENTE GUARDADO EN SESION
+        // CloseableHttpClient cliente = (CloseableHttpClient) session.getAttribute("cliente");
+        //------------------------------------------------------
 
         // TIPO DE CONTENIDO
         response.setContentType("text/html");
@@ -41,23 +61,36 @@ public class MenuAlumno extends HttpServlet {
 
         // SOLICITUD
         // URL
-        String milogin = http_req.getLocalName();
-        String dir = "http://" + milogin + ":9090/CentroEducativo/login";
+        String milogin = request.getLocalName();
+        String dir = "http://" + milogin + ":9090/CentroEducativo/alumnos/" + user + "?key=" + key;
+        out.println(key);
+        out.println(dir);
 
-        // POST
-        HttpPost post = new HttpPost(dir);
+        // GET
+        HttpGet get = new HttpGet(dir);
 
         // EJECUTO LA SOLICITUD
-        final CloseableHttpResponse execute = cliente.execute(post);
+        final CloseableHttpResponse execute = cliente.execute(get);
+        
+        // OBTENEMOS INFO
+        String info;
+		try {
+			info = EntityUtils.toString(execute.getEntity());
+			// COMPROBACION
+	        out.println(info);
+		} catch (ParseException | IOException e) {
+			e.printStackTrace();
+		}
+        
 
         // CERRAMOS RESPUESTA Y CLIENTE
         execute.close();
         cliente.close();
-    }
+        
+	}
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        doGet(request, response);
-    }
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
 
 }
